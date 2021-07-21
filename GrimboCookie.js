483b840bc98f70059a5cdff89e9748863bb40e5c
Walter-Kum-Dorne <Walter-Kum-Dorne@users.noreply.github.com>
2021-07-21 10:37:56 +0200

    Update GrimboCookie.js

--- GrimboCookie.js
+++ GrimboCookie.js
@@ -6,9 +6,9 @@ Game.registerMod(GrimboCookie.name, GrimboCookie);
 
 if (!Game.mods["Fortune Cookie"]) Game.LoadMod('https://klattmose.github.io/CookieClicker/FortuneCookie.js');
 
-var LoopShimmers;
-var LoopCombo;
-var LoopRefill;
+var GrimboCookie.LoopShimmers;
+var GrimboCookie.LoopCombo;
+var GrimboCookie.LoopRefill;
 
 GrimboCookie.Shimmers = function() {
 	
@@ -25,19 +25,19 @@ GrimboCookie.Combo = function() {
 	
 	if (Game.hasBuff('Frenzy') && Game.buffs['Frenzy'].time/Game.fps >= 30 && (Game.hasBuff('High-five') && Game.buffs['High-five'].time/Game.fps >= 30 || Game.hasBuff('Congregation') && Game.buffs['Congregation'].time/Game.fps >= 30 || Game.hasBuff('Luxuriant harvest') && Game.buffs['Luxuriant harvest'].time/Game.fps >= 30 || Game.hasBuff('Ore vein') && Game.buffs['Ore vein'].time/Game.fps >= 30 || Game.hasBuff('Oiled-up') && Game.buffs['Oiled-up'].time/Game.fps >= 30 || Game.hasBuff('Juicy profits') && Game.buffs['Juicy profits'].time/Game.fps >= 30 || Game.hasBuff('Fervent adoration') && Game.buffs['Fervent adoration'].time/Game.fps >= 30)) {
 		if (Gambler.indexOf('Click Frenzy') == 119) {
-			clearInterval(LoopCombo);
+			clearInterval(GrimboCookie.LoopCombo);
 			M.castSpell(M.spellsById[6]);
 			Game.shimmers[0].pop();
 			setTimeout(GrimboCookie.Gain, 3000);
 			setTimeout(GrimboCookie.StartCombo, 30000);
-			var LoopRefill = setInterval(GrimboCookie.Refill, 1000);
+			var GrimboCookie.LoopRefill = setInterval(GrimboCookie.Refill, 1000);
 		} else if (FTHoF == "<td><span style=\"color:#4BB8F0;\">Click Frenzy</span><br/></td>") {
-			clearInterval(LoopCombo);
+			clearInterval(GrimboCookie.LoopCombo);
 			M.castSpell(M.spellsById[1]);
 			Game.shimmers[0].pop();
 			setTimeout(GrimboCookie.Gain, 3000);
 			setTimeout(GrimboCookie.StartCombo, 30000);
-			var LoopRefill = setInterval(GrimboCookie.Refill, 1000);
+			var GrimboCookie.LoopRefill = setInterval(GrimboCookie.Refill, 1000);
 		}
 	}
 }
@@ -61,7 +61,7 @@ GrimboCookie.Refill = function() {
 			M.castSpell(M.spellsById[1]);
 			Game.shimmers[0].pop();
 		} else if (Gambler.indexOf('Click Frenzy') == 119 || FTHoF == "<td><span style=\"color:#4BB8F0;\">Click Frenzy</span><br/></td>") {
-			clearInterval(LoopRefill);
+			clearInterval(GrimboCookie.LoopRefill);
 		} else if (Gambler.indexOf('Spontaneous Edifice (Nothing)') == 95 || Gambler.indexOf('Resurrect Abomination') == 95 || Gambler.indexOf('Resurrect Abomination') == 93) {
 			M.castSpell(M.spellsById[6]);
 		} else if (Object.keys(Game.buffs).length == 0 && (Gambler.indexOf('Stretch Time') == 95 || Gambler.indexOf('Stretch Time') == 93)) {
@@ -76,9 +76,9 @@ GrimboCookie.Refill = function() {
 
 GrimboCookie.StartCombo = function() {
 	
-	var LoopCombo = setInterval(GrimboCookie.Combo, 3000);
+	var GrimboCookie.LoopCombo = setInterval(GrimboCookie.Combo, 3000);
 }
 
-var LoopShimmers = setInterval(GrimboCookie.Shimmers, 500);
-var LoopRefill = setInterval(GrimboCookie.Refill, 1000);
-var LoopCombo = setInterval(GrimboCookie.Combo, 3000);
+var GrimboCookie.LoopShimmers = setInterval(GrimboCookie.Shimmers, 500);
+var GrimboCookie.LoopRefill = setInterval(GrimboCookie.Refill, 1000);
+var GrimboCookie.LoopCombo = setInterval(GrimboCookie.Combo, 3000);
